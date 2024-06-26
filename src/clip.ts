ddccd403807c47fa189c4f5d5e6bdc6326d08502
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-06-26 11:56:35 -0600

    Force lazy-loaded images to load

--- src/clip.ts
+++ src/clip.ts
@@ -13,7 +13,17 @@ async function fetchPage(url: URL): Promise<JSDOM> {
 		throw new Error(`Failed to fetch ${url.toString()}`);
 	}
 	const page = new JSDOM(await response.text(), { url: url.toString() });
-
+	// force lazy-loaded images to load
+	const LAZY_DATA_ATTRS = ['data-src', 'data-lazy-src', 'data-srcset', 'data-td-src-property'];
+  for (const dataAttr of LAZY_DATA_ATTRS) {
+    const images = page.window.document.querySelectorAll(`img[${dataAttr}]`);
+    for (const img of images) {
+      const src = img.getAttribute(dataAttr);
+      if (src) {
+        img.setAttribute('src', src);
+      }
+    };
+  };
 	return page;
 }
 
