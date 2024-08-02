1c158ef723c7917db5fb0a57347f0a97a9ce1957
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-08-02 10:44:45 -0600

    Switch to Node from Bun
    @nurodev/astro-adapter-bun or whatever has a bug
    where it would throw a router error, but only
    inside the Docker container

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import bun from "@nurodev/astro-bun";
+import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
@@ -6,6 +6,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
-	adapter: bun(),
+	adapter: node({
+		mode: "standalone",
+	}),
 	integrations: [tailwind()],
 });
