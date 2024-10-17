99ec04b57fa9427ae921a9c439b016eff2737af2
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-10-17 12:25:13 -0600

    Change buttons to icons and move them to the side

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
 	adapter: node({
 		mode: "standalone",
 	}),
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		icon({
+			include: {
+				lucide: ["notebook-text", "file-type"],
+				local: ["inbox-download", "attach"],
+			},
+		}),
+	],
 });
