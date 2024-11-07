5b081910509feca93eb839f7ac2e7bb7e114c266
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-11-07 09:06:57 -0700

    Fix recent article styling (#16)
    * Fix recent article card width
    
    * Fix recent article card height
    
    * Fix recent article card url overflow

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const recentArticleCardClasses = [
+	"lg:w-40",
+	"md:w-60",
+	"flex-auto",
+	"p-2",
+	"bg-canvas",
+	"overflow-hidden",
+	"transition",
+	"rounded",
+	"border",
+	"hover:brightness-95",
+	"dark:hover:brightness-125",
+	"active:brightness-105",
+	"dark:active:brightness-90",
+].join(" ");
