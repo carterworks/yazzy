243ef0c5724136068fdc9685f333e910aa76e92d
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-11-07 08:48:07 -0700

    Fix recent article card width

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const recentArticleCardClasses = [
+	"lg:w-40",
+	"md:w-60",
+	"flex-auto",
+	"lg:h-64",
+	"p-2",
+	"bg-canvas",
+	"overflow-hidden",
+	"transition",
+	"rounded",
+	"border",
+	"hover:brightness-95",
+	"dark:hover:brightness-125",
+	"active:brightness-105",
+	"dark:active:brightness-90",
+].join(" ");
