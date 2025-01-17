9085d09851906ae64dbfc9ab1417e897ff0232a4
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-01-17 14:53:54 -0700

    Fix input bottom border

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,11 @@
 import type { FC } from "hono/jsx";
 
-const Input: FC<{ classes?: string }> = ({ classes = "", ...props }) => {
+const Input: FC<{
+	classes?: string;
+	[k: string]: string | boolean | undefined;
+}> = ({ classes = "", ...props }) => {
 	const className = [
-		"border-b border-base-100 dark:border-base-900 active:border-b-0 block w-full focus:ring-0 py-1 px-4 transition",
+		"border-b border-base-100 dark:border-base-900 block w-full py-1 px-4 transition",
 		classes,
 	].join(" ");
 	return <input className={className} {...props} />;
