9247e997e24b0b9323b1b59fab461b0c1b7deaa2
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-04-08 12:53:21 -0600

    Add JS only util class

--- src/static/scripts.mjs
+++ src/static/scripts.mjs
@@ -1,27 +1,6 @@
-window.addNotification = function addNotification(message) {
-	const $notificationsList = document.getElementById("notifications");
-	if (!$notificationsList) {
-		return;
+window.addEventListener("DOMContentLoaded", () => {
+	const $jsOnly = document.querySelectorAll(".js-only");
+	for (const $element of $jsOnly) {
+		$element.classList.remove("js-only");
 	}
-	const $notification = document.createElement("div");
-	$notification.setAttribute("role", "alert");
-	$notification.classList.add(
-		"fixed",
-		"rounded",
-		"bottom-0",
-		"right-0",
-		"p-4",
-		"m-4",
-		"drop-shadow",
-		"border",
-		"bg-paper",
-		"border-base-100",
-		"dark:bg-base-900",
-		"dark:border-base-900",
-	);
-	$notification.textContent = message;
-	$notificationsList.appendChild($notification);
-	setTimeout(() => {
-		$notification.remove();
-	}, 5000);
-};
+});
