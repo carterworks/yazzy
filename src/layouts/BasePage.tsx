177c16ba36d26c5e3f4e4027b3fc34acb609af0a
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-06-09 10:18:09 -0600

    Add header and footer spacing

--- src/layouts/BasePage.tsx
+++ src/layouts/BasePage.tsx
@@ -37,7 +37,7 @@ const BasePage: FC<PropsWithChildren<BasePageProps>> = ({
 			/>
 		</head>
 
-		<body className="text-base bg-paper font-geometric-humanist dark:bg-black text-black dark:text-base-200 px-4 py-2 space-y-2 max-w-prose mx-auto">
+		<body className="text-base bg-paper font-geometric-humanist dark:bg-black text-black dark:text-base-200 px-4 py-2 space-y-8 max-w-prose mx-auto">
 			<header
 				className="flex items-center gap-4 print:hidden"
 				style={{ viewTransitionName: "header" }}
