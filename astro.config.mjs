10291e9934050b2e3cf07d406a75d8792ec66631
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-12-17 09:32:26 -0700

    Use tailwind directly instead of tailwind cli

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server", // Full SSR
+	// Full SSR
+	output: "server",
+
 	adapter: node({
 		mode: "standalone",
 	}),
+
 	server: {
 		host: true,
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
