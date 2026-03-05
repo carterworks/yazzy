ef587ca35d30a57f766ee53fc0e7240dcbf0af90
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-03-05 15:18:46 -0700

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import node from "@astrojs/node";
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
